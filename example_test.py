d1c0c44e33c82bb2ee4081aa9bff09a5030c3f40
Zak <yozo1984@gmail.com>
2013-07-31 00:02:32 -0700

    A example that makes more sense

---
+++ example_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#
+# Tests for example module.
+import example
+from jazz import jazz
+
+
+class HelloTest(jazz.Describe):
+
+  def before_each(self):
+    example.ioc.SetTestMode()
+
+  def it_should_hello(self):
+    msg = example.Hello(greet='Greeting', app_name='Hello Method', user='Test User')
+    jazz.expect(msg).toEqual('<p>Hello Method: Greeting Test User</p>')
+
+
+if __name__ == '__main__':
+  jazz.run()
